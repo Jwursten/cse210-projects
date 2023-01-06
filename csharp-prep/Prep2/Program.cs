d6e58ced301f76e51a205773dfc10b2644737871
John Wursten <jwursten@gmail.com>
2023-01-06 12:30:02 -0700

    C# prep 2 complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string grade = Console.ReadLine();
+        int gradeNumber = int.Parse(grade);
+
+        string letterGrade = "";
+
+        if (gradeNumber >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradeNumber >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradeNumber >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradeNumber >= 60)
+        {
+            letterGrade = "D";
+        }
+        else if (gradeNumber < 60)
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letterGrade}");
+
+        if (gradeNumber >= 70)
+        {
+            Console.WriteLine("Congrats! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Do better next time. You failed.");
+        }
     }
 }
